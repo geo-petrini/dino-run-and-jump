9df5535975cde91cda9228456c6c14ab99595d73
geo-petrini <geo.petrini@gmail.com>
2024-03-04 20:58:20 +0100

    alert with decreasing progress bar

--- static/js/alert.js
+++ static/js/alert.js
@@ -18,9 +18,38 @@ function addAlertContainer() {
     document.body.innerHTML += ct;
 }
 
+
+function createProgressBar() {
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress");
+    progressBar.style.height = "1px";
+    // progressBar.style.direction = "rtl"; // TODO check if it works with bootstrap 5
+    progressBar.innerHTML = `
+        <div class="progress-bar bg-dark" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+    `;
+    return progressBar;
+}
+
+function updateProgressBar(progressBar, progress) {
+    progressBar.style.width = `${progress}%`;
+}
+
+function startProgressBarInterval(progressBar, newAlert, timeoutId) {
+    let progress = 100;
+    return setInterval(() => {
+        progress -= (50 / 3000) * 100;
+        updateProgressBar(progressBar, progress);
+
+        if (progress <= 0) {
+            clearInterval(intervalId);
+            removeAlert(newAlert);
+            alertTimeouts = alertTimeouts.filter(id => id !== timeoutId);
+        }
+    }, 50);
+}
+
 function removeAlert(alertElement) {
     alertElement.remove();
-
 }
 
 function displayAlert(message, level = "danger") {
@@ -28,15 +57,8 @@ function displayAlert(message, level = "danger") {
         addAlertContainer()
     }
 
-    // let newAlert = `
-    // <li class="list-group-item alert alert-${level}" alert-dismissible fade show" role="alert" >
-    //     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    //         <span aria-hidden="true">&times;</span>
-    //     </button>
-    //     ${message}
-    // </li>`
     let newAlert = document.createElement("li");
-    newAlert.classList.add("list-group-item", "alert", `alert-${level}`);
+    newAlert.classList.add("list-group-item", "alert", `alert-${level}`, "alert-dismissible");
     newAlert.innerHTML = `
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
@@ -45,22 +67,26 @@ function displayAlert(message, level = "danger") {
     `;    
 
     const alertsContainer = document.getElementById("alerts");
-    // alertsContainer.innerHTML += newAlert;
     alertsContainer.appendChild(newAlert);
 
-    // const lastAlert = alertsContainer.lastElementChild;
+    const progressBar = createProgressBar();
+    newAlert.appendChild(progressBar);    
+
     const lastAlert = alertsContainer.querySelector('.list-group-item:last-child');
 
+    let intervalId = null;
     // Rimuovi il messaggio dopo 3 secondi
     const timeoutId = setTimeout(() => {
+        clearInterval(intervalId);
         removeAlert(lastAlert);
         // Rimuovi il timeout dalla lista
         alertTimeouts = alertTimeouts.filter(id => id !== timeoutId);
-        console.log(alertTimeouts)
+        // console.log(alertTimeouts)
     }, 3000);
 
+    intervalId = startProgressBarInterval(progressBar, newAlert, timeoutId);
     // Aggiungi l'ID del timeout alla lista
     alertTimeouts.push(timeoutId); 
-    console.log(alertTimeouts)
+    // console.log(alertTimeouts)
 }
 
